b49bc9b177c6ac64d80b0c4776eef80ed189af29
Someone <rgbjkbs@hdbf.ru>
2018-04-23 20:26:16 +0300

    внес изменения в index.php создал AbstractModel.php

--- core/models/AbstractModel.php
+++ core/models/AbstractModel.php
@@ -1,6 +1,49 @@
 <?php
 
-
+namespace core\models;
+use mysqli;
 abstract class AbstractModel {
     
+    /**
+     *
+     * @var mysqli
+     */
+    protected $db;
+
+    /**
+     *
+     * @var string 
+     */
+    public $table;
+
+    public function __construct() {
+	$this->db = new mysqli('localhost', 'root', '', 'tasklist');
+    }
+
+    public function all() {
+	$query = "SELECT * FROM " . $this->table;
+	$result = $this->db->query($query);
+	if ($result) {
+	    return $result->fetch_all(MYSQLI_ASSOC);
+	}
+	return false;
+    }
+
+//    public function selectByID($id) {
+//	$query = "SELECT * FROM " . $this->table . " WHERE ID=" . $id." LIMIT 1";
+//	$result = $this->db->query($query);
+//	if ($result) {
+//	    return $result->fetch_object();
+//	}
+//	return false;
+//    }
+//    public function selectByName($name) {
+//	$query = "SELECT * FROM " . $this->table . " WHERE login='".$name."' LIMIT 1";
+//	$result = $this->db->query($query);
+//	if ($result) {
+//	    return $result->fetch_object();
+//	}
+//	return false;
+//    }
+
 }
