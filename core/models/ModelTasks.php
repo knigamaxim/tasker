b3622f9b94226e535820958e1333c767a9c5bef8
master <qewrqw@dwfds.com>
2018-04-23 20:42:29 +0300

    ModelTasks.php

---
+++ core/models/ModelTasks.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * класс для работы с ТАSKS-заданиями
+ */
+class ModelTasks extends AbstractModel{
+    
+     public function __construct() {
+	parent::__construct();
+        /**
+         * работаем с табл tasks нашей БД
+         */
+	$this->table = 'tasks';
+    }
+    
+//    public function addNews($news) {
+//	$query="INSERT INTO ".$this->table." (name, short_text, text, date) VALUES ('".$news['name']."', '".$news['short_text']."', '".$news['text']."', '".$news['date']."')";	
+//	return $this->db->query($query);
+//    }
+   
+
+    
+    
+    
+}
