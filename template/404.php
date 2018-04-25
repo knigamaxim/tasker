127a3f76b155848b8dcdf959109fca23a9e0d6af
somebody <somebody@smd.com>
2018-04-25 18:42:25 +0300

    Добавил страницу 404

---
+++ template/404.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Страница не найдена</title>
+    </head>
+    <body>
+	<img src="/images/page_not_found.jpg" alt="page not found" style="width: 100%"/>
+    </body>
+</html>
